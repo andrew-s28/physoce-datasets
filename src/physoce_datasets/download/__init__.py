a0944f1920dd387d19a0542a210db6c1c52fb0e9
Andrew Scherer <ascherer97@gmail.com>
2026-07-02 12:29:44 -0700

    add available download modules to download __init__

--- src/physoce_datasets/download/__init__.py
+++ src/physoce_datasets/download/__init__.py
@@ -1,1 +1,5 @@
 """Module for downloading physical oceanographic datasets and calculating derived quantities."""
+
+from . import copernicus_marine, era5, ooi_ea
+
+__all__ = ["copernicus_marine", "era5", "ooi_ea"]
